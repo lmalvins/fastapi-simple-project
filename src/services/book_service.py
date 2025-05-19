da628c0b81e99191538814f0be187dc6e3171cda
luisalvins <lmalvins@gmail.com>
2025-05-19 13:23:57 +0200

    adding an extra book to the list

--- src/services/book_service.py
+++ src/services/book_service.py
@@ -9,4 +9,5 @@ class BookService:
             Book(title="Title One", author="Author One", category="Science"),
             Book(title="Title Two", author="Author Two", category="Fiction"),
             Book(title="Title Three", author="Author Three", category="Adventure"),
+            Book(title="Title Four", author="Author Four", category="Terror"),
         ]
