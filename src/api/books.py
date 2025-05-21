b043f3ed92c25977e80450f0b367d6d43dfae4da
luisalvins <lmalvins@gmail.com>
2025-05-21 19:58:45 +0200

    fix: Adding endpoints to get books by query params

--- src/api/books.py
+++ src/api/books.py
@@ -6,12 +6,20 @@ router = APIRouter()
 book_service = BookService()
 
 @router.get("/books")
-def get_books():
+def get_all_books():
     return book_service.get_books()
 
 @router.get("/books/{book_title}")
-def get_books(book_title: str):
+def get_books_by_title(book_title: str):
     for book in book_service.get_books():
         if book.title.casefold() == book_title.casefold():
             return book
     raise HTTPException(status_code=404, detail="Book not found")
+
+@router.get("/books/{book_author}/")
+def get_books_by_category_author(category: str, book_author: str):
+    book_list = []
+    for book in book_service.get_books():
+        if book.category.casefold() == category.casefold() and book.author.casefold() == book_author.casefold():
+            book_list.append(book)
+    return book_list
